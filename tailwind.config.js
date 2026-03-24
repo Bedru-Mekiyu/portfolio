b1c393a073391360a48a6d488d1395e524ca78eb
bedru mekiyu <bedru.mekiyu-ug@aau.edu.et>
2026-03-24 10:11:51 +0300

    fixed the sidbar navigation and also remove the tailwind problem

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./assets/js/**/*.js",
+    "./style/**/*.js"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
